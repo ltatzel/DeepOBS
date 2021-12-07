b629f7bd160fc18643ebfaffc6483b4ec30d1ab4
Lukas Tatzel <lukas-nicola.tatzel@uni-tuebingen.de>
2021-12-07 16:16:57 +0100

    [ADD, REF] Added some more documentation and additional integration tests with BackPACK

--- deepobs/pytorch/testproblems/cifar10_resnet32.py
+++ deepobs/pytorch/testproblems/cifar10_resnet32.py
@@ -1,6 +1,19 @@
-# -*- coding: utf-8 -*-
-"""A resnet architecture for CIFAR-10.
-https://github.com/akamaster/pytorch_resnet_cifar10/blob/master/resnet.py
+"""Here, we implement the ``cifar10_resnet32`` testproblem that uses a 
+resnet-architecture. The original paper that introduced resnets can be found 
+here: https://arxiv.org/abs/1512.03385
+
+The code is copied from Yerlan Idelbayev's git repo "Proper ResNet 
+Implementation for CIFAR10/CIFAR100 in Pytorch", see
+https://github.com/akamaster/pytorch_resnet_cifar10 (accessed December 7th, 
+2021). 
+
+We use the following adaptions:
+- Added some documentation (e.g. some docstrings)
+- Replaced in-place operation in residual block (for BackPACK compatibility)
+
+TODO
+- Replace batch-normalization layers by group normalization, because per-sample
+quantities don't make sense with batch-normalization.  
 """
 
 import torch.nn as nn
@@ -12,13 +25,15 @@ from .testproblem import UnregularizedTestproblem
 
 
 def _weights_init(m):
-    # classname = m.__class__.__name__
-    # print(classname)
+    """Initialization for linear and conv layers"""
     if isinstance(m, nn.Linear) or isinstance(m, nn.Conv2d):
         init.kaiming_normal_(m.weight)
 
 
 class LambdaLayer(nn.Module):
+    """A layer that implements an arbitrary function ``lambd`` (specified at
+    instantiation)"""
+
     def __init__(self, lambd):
         super(LambdaLayer, self).__init__()
         self.lambd = lambd
@@ -28,25 +43,36 @@ class LambdaLayer(nn.Module):
 
 
 class BasicBlock(nn.Module):
+    """The basic block consists of two convolutional blocks, each followed by
+    a Batch-Norm layer and the ReLu activation function.
+
+    For option ``A`` (which is used with CIFAR-10), the shortcut/skip-connection
+    is the identity with extra zero padding. It basically adds the original
+    input ``x`` to the input of the second ReLu activation.
+    """
+
     expansion = 1
 
     def __init__(self, in_planes, planes, stride=1, option="A"):
         super(BasicBlock, self).__init__()
+
+        # First conv- and batch-normalization layer
         self.conv1 = nn.Conv2d(
             in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False
         )
         self.bn1 = nn.BatchNorm2d(planes)
+
+        # Second conv- and batch-normalization layer
         self.conv2 = nn.Conv2d(
             planes, planes, kernel_size=3, stride=1, padding=1, bias=False
         )
         self.bn2 = nn.BatchNorm2d(planes)
 
+        # The shortcut/skip-connection
         self.shortcut = nn.Sequential()
         if stride != 1 or in_planes != planes:
             if option == "A":
-                """
-                For CIFAR10 ResNet paper uses option A.
-                """
+                """For CIFAR10 ResNet paper uses this option A."""
                 self.shortcut = LambdaLayer(
                     lambda x: F.pad(
                         x[:, :, ::2, ::2],
@@ -70,12 +96,16 @@ class BasicBlock(nn.Module):
     def forward(self, x):
         out = F.relu(self.bn1(self.conv1(x)))
         out = self.bn2(self.conv2(out))
-        out += self.shortcut(x)
+
+        # Note: In-place operations are not compatible with BackPACK
+        out = out + self.shortcut(x)
         out = F.relu(out)
         return out
 
 
 class ResNet(nn.Module):
+    """The residual network consists of ``BasicBlock``s."""
+
     def __init__(self, block, num_blocks, num_classes=10):
         super(ResNet, self).__init__()
         self.in_planes = 16
@@ -110,26 +140,22 @@ class ResNet(nn.Module):
 
 
 class net_cifar10_resnet32(ResNet):
+    """The resnet with 32 layers"""
+
     def __init__(self):
         super().__init__(BasicBlock, [5, 5, 5], num_classes=10)
 
 
 class cifar10_resnet32(UnregularizedTestproblem):
-    """TODO"""
+    """DeepOBS testproblem (without regularization) for CIFAR-10 using a resnet
+    with 32 layers.
+    """
 
-    def __init__(self, batch_size, l2_reg=0.002):
-        """Create a new 3c3d test problem instance on Cifar-10.
-
-        Args:
-            batch_size (int): Batch size to use.
-            l2_reg (float): L2-regularization factor. L2-Regularization (weight decay)
-                is used on the weights but not the biases. Defaults to ``0.002``.
-        """
-
-        super(cifar10_resnet32, self).__init__(batch_size, l2_reg)
+    def __init__(self, batch_size):
+        super(cifar10_resnet32, self).__init__(batch_size)
 
     def set_up(self):
-        """Set up the vanilla CNN test problem on Cifar-10."""
+        """Set up the testproblem, i.e. (data, loss_function and network)"""
         self.data = cifar10(self._batch_size)
         self.loss_function = nn.CrossEntropyLoss
         self.net = net_cifar10_resnet32()
--- tests/pytorch/testproblems/test_cifar10_resnet32.py
+++ tests/pytorch/testproblems/test_cifar10_resnet32.py
@@ -1,15 +1,67 @@
-from deepobs.pytorch.testproblems.cifar10_resnet32 import net_cifar10_resnet32
+"""Test the CIFAR-10 testproblem with the resnet32 by checking that BackPACK's
+extensions and ViViT works with this architecture. Also check compatibility with
+DeepOBS by running the StandardRunner.
+"""
 
-"""Example run script using StandardRunner."""
-
+import torch
+from backpack import extend
 from torch.optim import SGD
 
 from deepobs import pytorch as pt
+from deepobs.pytorch.testproblems.cifar10_resnet32 import (
+    cifar10_resnet32,
+    net_cifar10_resnet32,
+)
 
+# from backpack.extensions import BatchGrad, DiagGGNExact
+
+
+# ------------------------------------------------------------------------------
+# Print some information about the resnet and check if it works with BackPACK
+# ------------------------------------------------------------------------------
+
+model = net_cifar10_resnet32()
+print("model = \n", model)
+
+# Get some CIFAR-10 data
+testproblem = cifar10_resnet32(batch_size=128)
+testproblem.set_up()
+train_loader, _ = testproblem.data._make_train_and_valid_dataloader()
+inputs, labels = next(iter(train_loader))
+print("inputs.shape = ", inputs.shape)
+print("labels.shape = ", labels.shape)
+
+# Put the model in evaluation mode (because batch normalization layers that are
+# not supported in train mode)
+model = model.eval()
+
+# Extend model and loss function
+lossfunc = extend(torch.nn.CrossEntropyLoss())
+# model = extend(model, use_converter=True)
+
+# Test first-order extention
+# print("\nTest first-order extension")
+# loss = lossfunc(model(inputs), labels)
+# with backpack(BatchGrad()):
+#    loss.backward()
+
+# Test second-order extension
+# print("\nTest second-order extension")
+# loss = lossfunc(model(inputs), labels)
+# with backpack(DiagGGNExact()):
+#     loss.backward()
+
+# Test ViViT
+# TODO
+
+
+# ------------------------------------------------------------------------------
+# Running an optimizer on the testproblem
+# ------------------------------------------------------------------------------
 optimizer_class = SGD
 hyperparams = {
-    "lr": {"type": float},
-    "momentum": {"type": float, "default": 0.99},
+    "lr": {"type": float, "default": 0.1},
+    "momentum": {"type": float, "default": 0.90},
     "nesterov": {"type": bool, "default": False},
 }
 
