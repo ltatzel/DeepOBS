7cfe4cfa297f42a450ac1efb79dd02fa988c1e72
Lukas Tatzel <lukas-nicola.tatzel@uni-tuebingen.de>
2022-01-01 14:08:14 +0100

    [REF] Replace ``view`` by ``Flatten``

--- deepobs/pytorch/testproblems/cifar10_resnet32.py
+++ deepobs/pytorch/testproblems/cifar10_resnet32.py
@@ -96,10 +96,13 @@ class ResNet(nn.Module):
         self.layer1 = self._make_layer(block, 16, num_blocks[0], stride=1)
         self.layer2 = self._make_layer(block, 32, num_blocks[1], stride=2)
         self.layer3 = self._make_layer(block, 64, num_blocks[2], stride=2)
-        self.linear = nn.Linear(64, num_classes)
 
         kernel_size = 8  # works for cifar-10, resnet32
         self.avgpool = nn.AvgPool2d(kernel_size)
+
+        self.flatten = nn.Flatten()
+
+        self.linear = nn.Linear(64, num_classes)
 
         self.apply(_weights_init)
 
@@ -118,7 +121,7 @@ class ResNet(nn.Module):
         out = self.layer2(out)
         out = self.layer3(out)
         out = self.avgpool(out)
-        out = out.view(out.size(0), -1)
+        out = self.flatten(out)
         out = self.linear(out)
         return out
 
