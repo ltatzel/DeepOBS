c263e39eeafbf640a620b1b7c030213d86d4cb50
Lukas Tatzel <lukas-nicola.tatzel@uni-tuebingen.de>
2022-11-14 14:41:13 +0100

    [REF] Simplify `set_weight_decay` (with test)

--- deepobs/pytorch/testproblems/imagenet_resnet50.py
+++ deepobs/pytorch/testproblems/imagenet_resnet50.py
@@ -5,7 +5,9 @@ might not work with other versions. It is extracted from:
 https://github.com/pytorch/vision/tree/release/0.13/references/classification
 """
 
-from torch import nn
+from copy import deepcopy
+
+from torch import allclose, nn
 from torchvision.models import resnet50
 
 from deepobs.pytorch.testproblems.testproblem import TestProblem
@@ -51,11 +53,125 @@ class imagenet_resnet50(TestProblem):
         self.regularization_groups = self.get_regularization_groups()
 
 
+def set_weight_decay_ref(
+    model,
+    weight_decay,
+    norm_weight_decay=None,
+    norm_classes=None,
+    custom_keys_weight_decay=None,
+):
+
+    if not norm_classes:
+        norm_classes = [
+            nn.modules.batchnorm._BatchNorm,
+            nn.LayerNorm,
+            nn.GroupNorm,
+            nn.modules.instancenorm._InstanceNorm,
+            nn.LocalResponseNorm,
+        ]
+    norm_classes = tuple(norm_classes)
+
+    params = {
+        "other": [],
+        "norm": [],
+    }
+    params_weight_decay = {
+        "other": weight_decay,
+        "norm": norm_weight_decay,
+    }
+    custom_keys = []
+    if custom_keys_weight_decay is not None:
+        for key, weight_decay in custom_keys_weight_decay:
+            params[key] = []
+            params_weight_decay[key] = weight_decay
+            custom_keys.append(key)
+
+    def _add_params(module, prefix=""):
+        for name, p in module.named_parameters(recurse=False):
+            if not p.requires_grad:
+                continue
+            is_custom_key = False
+            for key in custom_keys:
+                target_name = (
+                    f"{prefix}.{name}" if prefix != "" and "." in key else name
+                )
+                if key == target_name:
+                    params[key].append(p)
+                    is_custom_key = True
+                    break
+            if not is_custom_key:
+                if norm_weight_decay is not None and isinstance(
+                    module, norm_classes
+                ):
+                    params["norm"].append(p)
+                else:
+                    params["other"].append(p)
+
+        for child_name, child_module in module.named_children():
+            child_prefix = (
+                f"{prefix}.{child_name}" if prefix != "" else child_name
+            )
+            _add_params(child_module, prefix=child_prefix)
+
+    _add_params(model)
+
+    param_groups = []
+    for key in params:
+        if len(params[key]) > 0:
+            param_groups.append(
+                {
+                    "params": params[key],
+                    "weight_decay": params_weight_decay[key],
+                }
+            )
+    return param_groups
+
+
+def set_weight_decay(
+    model,
+    weight_decay,
+):
+
+    params = []
+    for _, p in model.named_parameters():
+        if not p.requires_grad:
+            continue
+        params.append(p)
+
+    return [
+        {
+            "params": params,
+            "weight_decay": weight_decay,
+        }
+    ]
+
+
 if __name__ == "__main__":
 
-    print("\n===== START =====")
+    print("\nSet up testproblem")
 
     tp = imagenet_resnet50()
     tp.set_up()
 
-    print("===== DONE =====\n")
+    # Test regularization
+    model_ref = resnet50(weights="IMAGENET1K_V1")
+    model = deepcopy(model_ref)
+    weight_decay = 1e-2
+
+    print("\nCall set_weight_decay_ref")
+    param_groups_ref = set_weight_decay_ref(model_ref, weight_decay)
+
+    print("Call set_weight_decay")
+    param_groups = set_weight_decay(model, weight_decay)
+
+    # Compare
+    print("Compare results")
+    assert len(param_groups) == len(param_groups_ref) == 1
+    params = param_groups[0]
+    params_ref = param_groups_ref[0]
+
+    assert len(params) == len(params_ref) == 2
+    assert params["weight_decay"] == params_ref["weight_decay"]
+    assert len(params["params"]) == len(params_ref["params"]) > 0
+    for p, p_ref in zip(params["params"], params_ref["params"]):
+        assert allclose(p, p_ref)
