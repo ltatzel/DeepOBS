283b06002f7d85bb48650f37e0fb6f96035508a3
ltatzel <lukas.tatzel@gmail.com>
2022-11-23 11:50:44 +0100

    [REF] Minor

--- deepobs/pytorch/testproblems/imagenet_resnet50.py
+++ deepobs/pytorch/testproblems/imagenet_resnet50.py
@@ -320,6 +320,9 @@ class EmptyDataset(torch.utils.data.IterableDataset):
     """An empty Pytorch dataset"""
     def __init__(self):
         super().__init__()
+
+    def __len__(self):
+        return 0
     
     def __iter__(self):
         return iter([])
